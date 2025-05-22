0388e360b47884dc05e79e0c04c0a201dd68aa2f
Thomas Minh Nguyen <thomasnguyen8503@gmail.com>
2025-05-23 09:53:07 +1200

    new collection called article created

---
+++ backend/speed_backend/src/api/articles/article.schema.ts
@@ -0,0 +1,49 @@
+/* eslint-disable prettier/prettier */
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Date, HydratedDocument } from 'mongoose';
+
+export type ArticleDocument = HydratedDocument<Article>;
+
+@Schema()
+export class Article {
+
+    // Articles Information
+    @Prop({ required: true })
+    title: string;
+    
+    @Prop({ required: true })
+    author: string;
+    
+    @Prop({ required: true })
+    journal_name: string;
+
+    @Prop({type: Date, required: true })
+    publication_date: Date;
+
+    @Prop({ required: true })
+    volume: string;
+
+    @Prop({ required: true })
+    number: string;
+
+    @Prop({ required: true })
+    pages: string;
+
+    @Prop({ required: true })
+    doi: string;
+
+    // Submission Information
+    @Prop({ type: Date, required: true })
+    submission_date: Date;
+
+    @Prop({ required: true })
+    summary_brief: string;
+
+    @Prop({ required: true })
+    moderation_pass: boolean;
+
+    @Prop({ required: true })
+    analysis_pass: boolean;
+}
+
+export const ArticleSchema = SchemaFactory.createForClass(Article);
