eda89ebcb72ce4e670eae72c126e22757a2cf3d7
Thomas Minh Nguyen <thomasnguyen8503@gmail.com>
2025-05-23 10:30:03 +1200

    added additional import for article module in app.module \n backend testing working

--- backend/speed_backend/src/app.module.ts
+++ backend/speed_backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import {MongooseModule} from '@nestjs/mongoose';
+import { ArticleModule } from './api/articles/article.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DB_URI as string)],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DB_URI as string), ArticleModule],
   controllers: [AppController],
   providers: [AppService],
 })
