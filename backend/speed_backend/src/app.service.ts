eb5e27b5f1984d6bae2db868c2184a2fe0a96036
719 <penelope.huang20000@gmail.com>
2025-05-23 21:32:42 +1200

    change

---
+++ backend/speed_backend/src/app.service.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+}
---
+++ backend/speed_backend/src/main.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({ origin: true, credentials: true });
+  
+  await app.listen(process.env.PORT ?? 8082);
+}
+bootstrap();
